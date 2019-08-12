7a47e7d9e021ef149eab2c30eec78510ef5ae0ce
venkat102 <47534423+venkat102@users.noreply.github.com>
2019-08-12 15:46:40 +0530

    Create even_odd.java

---
+++ Begginer/Set 1/even_odd.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class even_odd {
+    public static void main(String args[])
+    {
+        int a;
+        Scanner s=new Scanner(System.in);
+        a=s.nextInt();
+        if(a<=0)
+        {
+            System.out.println("Invalid");
+        }
+        else if(a%2!=0)
+        {
+            System.out.println("Odd");
+        }
+        else if(a%2==0)
+        {
+            System.out.println("Even0");
+        }
+        
+    }
+    
+}
