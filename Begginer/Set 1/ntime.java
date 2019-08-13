c9951eb7eb83e88c2729dca1b52bd34d28472f0e
venkat102 <47534423+venkat102@users.noreply.github.com>
2019-08-13 07:19:53 +0530

    Create ntime.java

---
+++ Begginer/Set 1/ntime.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ntime {
+    public static void main(String args[])
+    {
+        int n;
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("Hello");
+        }
+    }
+}
