fe879de3829a680994ae3d9c7c6ee242ef832664
venkat102 <47534423+venkat102@users.noreply.github.com>
2019-08-13 20:22:33 +0530

    Create even_interval.java

---
+++ Begginer/Set 2/even_interval.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class even_interval {
+    public static void main(String args[])
+    {
+        int a,b;
+        Scanner s=new Scanner(System.in);
+        a=s.nextInt();
+        b=s.nextInt();
+        for(int i=++a;i<b;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
