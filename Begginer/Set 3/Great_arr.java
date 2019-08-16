cde605ce486d164290561b3bc4c17797a442dc01
venkat102 <47534423+venkat102@users.noreply.github.com>
2019-08-16 08:37:28 +0530

    Create Great_arr.java

---
+++ Begginer/Set 3/Great_arr.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+
+ class Great_arr
+ {
+ 	public static void main(String args[])
+ 	{ 
+	  
+	 Scanner s=new Scanner(System.in);
+	 int n=s.nextInt();
+	 int a[]=new int[n];
+	 for(int i=0;i<n;i++)
+	 {
+ 		a[i]=s.nextInt();
+ 	}
+ 	for(int i=0;i<n;i++)
+ 	{
+ 		for(int j=i+1;j<n;j++)
+ 		{
+ 			if(a[i]>a[j])
+ 			{
+ 				a[i]=a[i]+a[j];
+ 				a[j]=a[i]-a[j];
+ 				a[i]=a[i]-a[j];
+ 			}
+ 		}
+ 	}
+ 	System.out.println(a[n-1]);
+ 	}
+ }
